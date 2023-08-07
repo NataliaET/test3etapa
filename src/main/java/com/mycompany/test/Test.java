693ee2a6fa9a05864b1e997517ab109916a12e8b
daniel-calvo <danielcst87@gmail.com>
2023-08-07 17:15:32 -0300

    Cambio de mensaje
    Cambie el mensaje "hola mundo" por "probando github"

--- src/main/java/com/mycompany/test/Test.java
+++ src/main/java/com/mycompany/test/Test.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.test;
 
-/**
- *
- * @author Asus
- */
+
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Probando GitHub");
     }
 }
